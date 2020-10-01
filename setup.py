75152b2f76b22953ad488c39b1de1360365bdba6
smith <smith@ca.ibm.com>
2020-10-01 10:57:04 -0400

    doing stuff

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from os import path
+
+here = path.abspath( path.dirname( __file__ ) )
+
+setup(
+    name='trish-eval-app',
+    version='1.0.0',
+    description='Python Flask app for evaluating Speech to Text results',
+    license='Apache-2.0'
+)
